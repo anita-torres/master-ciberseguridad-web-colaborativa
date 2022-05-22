1e51e796821630c10096caf8a519f483de70a078
Anit Torres <anita.torres.76@gmail.com>
2022-05-22 18:06:55 -0400

    Corrige sobrescribir usuario al registrar

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -4,6 +4,7 @@ package controllers;
 import helpers.HashUtils;
 import models.User;
 import play.mvc.Controller;
+import play.i18n.Messages;
 
 public class PublicContentBase extends Controller {
 
@@ -12,9 +13,15 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        User u = User.loadUser(username);
+        if (u != null){
+            flash.put("error", Messages.get("Public.register.validation.existingAccount"));
+            register();
+        }else{
+            u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        }
     }
 
     public static void registerComplete(){
